b97afc68355565b4543c93df1c664f135667f2b3
ustimenkotheking <dania200211@icloud.com>
2021-03-09 16:36:53 +0300

    qwerty

--- main.cpp
+++ main.cpp
@@ -1,6 +1,80 @@
 #include <iostream>
 
+class Item {
+    const int index;
+    int value;
+public:
+    static int currentSize;
+    Item(int index, int value) : index(index) {
+        this->value = value;
+    }
+    Item(const Item& other) : index(other.index) {
+        value = other.value;
+    }
+    ~Item() {
+        /* nothing to do here */
+    }
+
+    Item& operator=(const Item& other) {
+        if (this == &other) {
+            return *this;
+        }
+
+        //index = other.index; //�� ����� ������� ���������
+        value = other.value;
+
+        return *this;
+    }
+
+};
+
+const unsigned K = 10;
+Item* items[K];
+int Item::currentSize = 0;
+
+//void add(Item* item) {
+//	if (currentSize < K) {
+//		items[currentSize] = item;
+//		currentSize++;
+//	}
+//}
+void add(int value) {
+    if (Item::currentSize < K) {
+        items[Item::currentSize] = new Item(Item::currentSize, value);
+        Item::currentSize++;
+    }
+}
+void clear() {
+    for (int i = 0; i < Item::currentSize; i++) {
+        delete items[i];
+        items[i] = nullptr;
+    }
+}
+
+Item createTest() {
+    Item tmp(0, 1);
+    return tmp;
+}
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    /*
+        clone
+        pull
+        commit
+        push
+
+
+        https://ravesli.com/urok-133-peregruzka-operatorov-vvoda-i-vyvoda/
+
+    */
+
+    add(100);
+    add(200);
+
+    Item &a = *items[0];
+    Item &b = *items[1];
+    a = b;
+
+    clear();
     return 0;
 }
